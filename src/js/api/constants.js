35714cb5cc07ba1940bad72230cb86f1164ef090
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2024-09-25 19:11:48 +0200

    Update constants.js
    chore: actually added api key

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -1,6 +1,6 @@
 // Use Postman, or JavaScript to get your API key
 // In Workflow we will learn how to secure this information
-export const API_KEY = "replace-with-your-key";
+export const API_KEY = "d5b92f07-8a9c-4bf8-a081-8748ef214771";
 
 export const API_BASE = "https://v2.api.noroff.dev";
 
@@ -17,3 +17,16 @@ export const API_SOCIAL = `${API_BASE}/social`;
 export const API_SOCIAL_POSTS = `${API_SOCIAL}/posts`;
 
 export const API_SOCIAL_PROFILES = `${API_SOCIAL}/profiles`;
+
+
+    // "name": "panidahelene", // Required
+    // "email": "panida.helene@stud.noroff.no", // Required
+    // "password": "panidahelene1234", // Required
+
+    // const options = {
+    //     headers: {
+    //       'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoicGFuaWRhaGVsZW5lIiwiZW1haWwiOiJwYW5pZGEuaGVsZW5lQHN0dWQubm9yb2ZmLm5vIiwiaWF0IjoxNzI3MTE0NzU3fQ.lFMR4IjpbFv4c6243Egpo7v3DG0Qx9eD7QouDUi8454',
+    //       'X-Noroff-API-Key': 'd5b92f07-8a9c-4bf8-a081-8748ef214771'
+    //     }
+    //   };
+  
