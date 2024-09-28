f5e5d3816b810b8a76278a2ab5483444620753d7
Helflaa <146756162+Helflaa@users.noreply.github.com>
2024-09-28 16:47:25 +0200

    feat: Implement post deletion functionality with API integration
    - Added `deletePost` function in `api/post/delete.js` to handle deleting posts via a DELETE request to the API.
    - Integrated authorization using API key and bearer token for secure deletion of posts.
    
    - Added success alert and redirection to the homepage upon successful post deletion.
    
    - Implemented error handling for failed deletion requests, with error logs and user notifications.

--- src/js/api/post/delete.js
+++ src/js/api/post/delete.js
@@ -1,1 +1,43 @@
-export async function deletePost(id) {}
+import { API_SOCIAL_POSTS, API_KEY } from "../constants";
+import {headers } from "../../api/headers";
+
+/**
+ * Deletes a post by sending a DELETE request to the social API.
+ * 
+ * This function sends a DELETE request to the API to remove the specified post.
+ * If the request is successful, it alerts the user and redirects to the homepage.
+ * If the request fails, it logs the error and displays an error message.
+ * 
+ * @async
+ * @param {number} postId - The ID of the post to be deleted.
+ * 
+ * @returns {Promise<void>} A promise that resolves if the post is deleted successfully.
+ * Displays a success message and redirects the user to the homepage.
+ * If an error occurs, it logs the error and displays an error message.
+ * 
+ * @throws Will throw an error if the network request fails or the server response is not ok.
+ */
+export async function deletePost(postId) {
+    const myHeaders = headers();
+
+    const requestOptions = {
+      method: "DELETE",
+      headers: myHeaders,
+    };
+
+    try {
+        const response = await fetch(`${API_SOCIAL_POSTS}/${postId}`, requestOptions);
+
+        if (response.ok) {
+            alert("Post deleted successfully");
+            window.location.assign("/");
+        } else {
+            console.error(result);
+            alert("Post could not be deleted: " + result.message);
+        }
+    } catch (error) {
+        console.error("Error while deleting post:", error);
+        alert("Failed to delete the post. Please try again later.");
+    }
+}
+
