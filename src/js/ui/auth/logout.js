61c6650fe79e41ae8d6f1904859f4306e550efff
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2024-10-02 02:14:22 +0200

    refactor : Moved the removal of localStorage items to onLogout function

--- src/js/ui/auth/logout.js
+++ src/js/ui/auth/logout.js
@@ -1,1 +1,13 @@
-export function onLogout() {}
+/**
+ * Logs out the current user by clearing authentication data and redirecting to the login page.
+ * 
+ * This function removes the 'accessToken' and 'userName' from the local storage,
+ * effectively ending the user's session. After clearing the data, it redirects
+ * the user to the login page to prompt for re-authentication.
+ */
+export function onLogout() {
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('userName');
+        window.location.href = '/auth/login/';
+    }
+    
