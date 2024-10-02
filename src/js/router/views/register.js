37345302cea188021a4e82435227e441ad29d5a2
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2024-10-02 02:13:33 +0200

    Chore : added jsdoc

--- src/js/router/views/register.js
+++ src/js/router/views/register.js
@@ -2,4 +2,9 @@ import { onRegister } from "../../ui/auth/register";
 
 const form = document.forms.register;
 
+/**
+ * Adds a submit event listener to the registration form.
+ * 
+ * When the form is submitted, the `onRegister` function is called to handle the registration process.
+ */
 form.addEventListener("submit", onRegister);
